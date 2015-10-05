87e3384427d8a45f9e70883aadb39b2c9eecb9cb
Julie David <julie.angela.david@gmail.com>
2015-10-05 17:28:06 -0400

    Menu sorted by courses, can add certain courses, can edit dishes by clicking their names, can delete dishes from edit page

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -15,7 +15,7 @@ class DishesController < ApplicationController
 
   # GET /dishes/new
   def new
-    @dish = Dish.new
+    @dish = Dish.new(course_id: params[:course_id])
   end
 
   # GET /dishes/1/edit
